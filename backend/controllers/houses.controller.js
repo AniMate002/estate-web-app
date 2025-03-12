eca0e156637850e530f3b534ae95534e7dcccc8c
Alukard <kirill1974111@gmail.com>
2025-03-12 12:31:02 +0100

    (feat): v0.0.3 Added proxy & Front-end fetch example

--- backend/controllers/houses.controller.js
+++ backend/controllers/houses.controller.js
@@ -8,7 +8,7 @@ export const getAllHouses = async (req, res) => {
     }catch(e){
         const errorMessage = e instanceof Error ? e.message : "Unknown error";
         console.log("Error in getAllHouses controller: ", errorMessage);
-        res.status(500).json({message: errorMessage});
+        res.status(500).json({error: errorMessage});
     }
 }
 
@@ -16,13 +16,13 @@ export const getHouseById = async (req, res) => {
     try {
         const { id } = req.params
         console.log("ID:", id)
-        if(!id) return res.status(400).json({message: "Error: No houseId provided"})
+        if(!id) return res.status(400).json({error: "Error: No houseId provided"})
         const house = await House.findById(id)
-        if(!house) return res.status(404).json({message: "House with such id was not found"})
+        if(!house) return res.status(404).json({error: "House with such id was not found"})
         return res.status(200).json(house)
     } catch (error) {
         const errorMessage = e instanceof Error ? e.message : "Unknown error";
         console.log("Error in getHouseById controller: ", errorMessage);
-        res.status(500).json({message: errorMessage});
+        res.status(500).json({error: errorMessage});
     }
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [houses, setHouses] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
+  const fetchHouses = async () => {
+    try{
+      setLoading(true)
+      const res = await fetch("/api/houses");
+      const data = await res.json()
+      if(!data) throw new Error("Data not recieved")
+      if(data.error) throw new Error(data.error)
+      setHouses(data)
+    }catch(e){
+      setError(e instanceof Error ? e.message : "Unknown error")
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchHouses()
+  }, [])
+
+  if(error) return <div>Error: {error}</div>
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        loading
+        ?
+        <h1>Loading</h1>
+        :
+        houses?.map(house => <div key={house.id}>{house.name}</div>)
+      }
     </>
   )
 }
