55f270ea2f5a12a1fef524b0bff4517b1d2a0292
Alukard <kirill1974111@gmail.com>
2025-03-12 14:41:06 +0100

    (feat): v0.0.5 User update route

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -23,3 +23,31 @@ export const getUserById = async (req, res) => {
         res.status(500).json({error: errorMessage});
     }
 }
+
+
+export const userUpdate = async (req, res) => {
+    try {
+        const { id: userId } = req.user;
+        const { name, email, avatar } = req.body;
+
+        const user = await User.findById(userId);
+        if(!user) return res.status(404).json({error: "User not found"});
+        
+        if(email !== user.email){
+            // IF USER CHANGES EMAIL, CHECK IF IT IS ALREADY TAKEN
+            const takenUser = await User.findOne({email});
+            if(takenUser) return res.status(400).json({error: "User with this email already exists"});
+        }
+
+        user.name = name || user.name;
+        user.email = email || user.email;
+        user.avatar = avatar || user.avatar;
+
+        await user.save()
+        return res.status(200).json(user);
+    } catch (e) {
+        const errorMessage = e instanceof Error ? e.message : "Unknown error";
+        console.log("Error in userUpdate controller: ", errorMessage);
+        res.status(500).json({error: errorMessage});
+    }
+}
--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllUsers, getUserById } from "../controllers/users.controller.js";
+import { getAllUsers, getUserById, userUpdate } from "../controllers/users.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/", getAllUsers)
-router.get("/:id", getUserById)
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.put("/", protectRoute, userUpdate);
 
 export default router;
